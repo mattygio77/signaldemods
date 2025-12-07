b03e83175a6be0d4f0e168ad0e2d5e3df51a2d23
Matt Giovannucci <matthewgiovannucci03@gmail.com>
2025-12-07 13:37:14 -0500

    Converting bpsk Demod to use plotting library

--- BPSK/bpskDemod.py
+++ BPSK/bpskDemod.py
@@ -1,82 +1,48 @@
 #!/usr/bin/env python3
+import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
 
+#Local imports
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+import plottingLib
+
 plt.style.use('dark_background')
 
-sample_rate, audio_data = wavfile.read('./BPSK_IQ_Fs48KHz.wav')
+sample_rate, audio_data = wavfile.read('./BPSK-Decode/BPSK_IQ_Fs48KHz.wav')
 
 print(f"Sample Rate: {sample_rate}")
 
 i_samples = audio_data[:, 0]
 q_samples = audio_data[:, 1]
 
-fig1, axs1 = plt.subplots(2, 1)
-axs1[0].plot(i_samples, label="In-phase (I)")
-axs1[0].plot(q_samples, label="Quadrature (Q)")
-axs1[0].set_xlabel("Sample Index")
-axs1[0].set_ylabel("Amplitude")
-axs1[0].set_title("I/Q Time-Domain Plot")
-axs1[0].legend()
-axs1[0].grid(True)
-
+# Compute FFT
 complex_signal = i_samples + 1j * q_samples
 fft_result = np.fft.fft(complex_signal)
 frequencies = np.fft.fftfreq(len(complex_signal), d=1/sample_rate)
 
-fig, axs = plt.subplots(2, 1)
-axs[0].plot(np.fft.fftshift(frequencies), np.fft.fftshift(np.abs(fft_result)))
-axs[0].set_title('FFT Magnitude of IQ Data')
-axs[0].set_xlabel('Frequency (Hz)')
-axs[0].set_ylabel('Magnitude')
-
+# Freq Adjust
 freqOffset = 4320
-phaseOffset = np.pi * -.2
 duration = len(i_samples)/sample_rate
 t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
 complexConjugate = np.exp(-1j * (2 * np.pi * freqOffset * t))
 freqAdjSig = complexConjugate * complex_signal
 
+# Phase Adjust
+phaseOffset = np.pi * -.2
 complexConjugatePhase = np.exp(-1j * (2 * np.pi * freqOffset * t + phaseOffset))
 phaseAdjSig = complexConjugatePhase * complex_signal
 
 fft_result = np.fft.fft(freqAdjSig)
 frequencies = np.fft.fftfreq(len(freqAdjSig), d=1/sample_rate)
 
-axs[1].plot(np.fft.fftshift(frequencies), np.fft.fftshift(np.abs(fft_result)))
-axs[1].set_title('FFT Magnitude of IQ Data')
-axs[1].set_xlabel('Frequency (Hz)')
-axs[1].set_ylabel('Magnitude')
-plt.tight_layout()
-
 freqAdjSigI = np.real(freqAdjSig)
 freqAdjSigQ = np.imag(freqAdjSig)
 
-axs1[1].plot(np.real(phaseAdjSig), label="In-phase (I)")
-# axs1[1].plot(np.imag(phaseAdjSig), label="Quadrature (Q)")
-axs1[1].set_xlabel("Sample Index")
-axs1[1].set_ylabel("Amplitude")
-axs1[1].set_title("I/Q Time-Domain Plot")
-axs1[1].legend()
-axs1[1].grid(True)
-# plt.show() 
-
-limits = 30e3
-plt.figure(figsize=(5, 5))
-plt.scatter(freqAdjSigI, freqAdjSigQ, s=10, alpha=0.7, label="Phase Offset")
-plt.title('IQ Constellation Diagram')
-plt.xlabel('In-phase (I)')
-plt.ylabel('Quadrature (Q)')
-plt.grid(True)
-plt.axhline(0, color='gray', linewidth=4)
-plt.axvline(0, color='gray', linewidth=4)
-plt.xlim(-limits, limits)
-plt.ylim(-limits, limits)
-
-plt.scatter(np.real(phaseAdjSig), np.imag(phaseAdjSig), s = 10, alpha=0.7, label="Phase Adjusted")
-plt.legend()
-
+# Decoding Frame
 spb = 40
 intDecision = []
 for n in range(int(phaseAdjSig.size/spb)):
@@ -91,4 +57,10 @@ for n in range(int(len(bits)/8)):
     ans.append(chr(byte_value))
 
 print(f"ans: {ans}")
+
+#Plotting
+plottingLib.plotTime(complex_signal, phaseAdjSig)
+plottingLib.plotFreq(complex_signal, sample_rate, phaseAdjSig)
+plottingLib.plotConstallation(freqAdjSig, phaseAdjSig)
+
 plt.show()
